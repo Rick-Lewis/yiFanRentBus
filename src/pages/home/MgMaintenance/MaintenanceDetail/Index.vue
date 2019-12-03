326deb23899d7284ab961c994499be7c882635c1
xu_long <xu_long@kingdee.com>
2019-12-03 14:22:11 +0800

    增加维保和维保详情

---
+++ src/pages/home/MgMaintenance/MaintenanceDetail/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="maintenance-detail-container"></div>
+</template>
+<script>
+export default {
+    name: 'MaintenanceDetail',
+    data: function(){
+        return {};
+    },
+    created(){},
+    mounted(){},
+    computed: {},
+    methods: {},
+    components: {}
+};
+</script>
+<style lang='scss' scoped>
+@import './Index.scss';
+</style>
